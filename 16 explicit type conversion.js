d0bfa6c2395ceda38d7fc7eaeb5a0d15a35c09ca
Jay Prakash Mangal <prabalmangal@gmail.com>
2021-11-29 15:23:49 +0530

    init

--- 16 explicit type conversion.js
+++ 16 explicit type conversion.js
@@ -4,3 +4,13 @@ console.log(typeof(String(true)))
 console.log(typeof(String(-98.10)))
 console.log(typeof(String(null)))
 console.log(typeof(String(undefined)))
+
+// number
+console.log(typeof(Number('2')))
+console.log(Number(true),typeof(Number(true)))
+console.log(Number(false),typeof(Number(true)))
+// boolean
+
+console.log(Boolean(1),typeof(Boolean(1)))
+console.log(Boolean(true),typeof(Boolean(true)))
+
---
+++ 17.oblects.js
@@ -0,0 +1,21 @@
+// var  mcars={
+//     "p1":"350km",
+//     "gallardo":'320km',
+//     "veryan" :'409km',
+//     "agera" : '429km'
+// }
+// console.log(mcars)
+// console.log(typeof(mcars))
+
+
+
+var Agera={
+    name:"Agera",
+manufacturer:{
+    name :"koienseige",
+    location: "swedaen"
+},
+topspeed :429,
+color :"black",
+spoilers: false
+}
