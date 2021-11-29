1de173db6bc30aae317b403c0e5400ff5160e2d3
Jay Prakash Mangal <prabalmangal@gmail.com>
2021-11-29 09:04:09 +0530

    init

---
+++ 16 explicit type conversion.js
@@ -0,0 +1,6 @@
+var Mnum=15;
+console.log(typeof(String(Mnum)))
+console.log(typeof(String(true)))
+console.log(typeof(String(-98.10)))
+console.log(typeof(String(null)))
+console.log(typeof(String(undefined)))
